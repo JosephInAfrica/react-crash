b4cccc03927bdc5f511aeee13e7761d0495c5eae
JosephInAfrica <lochleee@gmail.com>
2024-05-18 01:05:43 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobs from "./components/ViewAllJobs";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/about' element={<h1>My App</h1>} />)
+  // createRoutesFromElements(<Route path='/' element={<h1>My App</h1>} />)
+
+);
 const App = () => {
-
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-      <script src="js/main.js"></script>
-    </>
-  )
+  return <RouterProvider router={router} />
+  // return (
+  //   <>
+  //     <Navbar />
+  //     <Hero />
+  //     <HomeCards />
+  //     <JobListings />
+  //     <ViewAllJobs />
+  //     <script src="js/main.js"></script>
+  //  </>
+  // )
 }
 
 export default App
